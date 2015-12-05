6c5781e3620c1b2249e9752d43ab26b85732adb1
circld <circld1@gmail.com>
2015-12-05 16:45:45 -0700

    working modularized mcmc function completed.

--- model.R
+++ model.R
@@ -1,194 +1,50 @@
-# set up simulation parameters
-totalPop <- 10000
+# MCMC implementation of Pan & Nagurney paper
 
-capacity <- c(5000, 5000)
+step.MCMC <- function(pop, utility, proposal.dist, move.cost) {
+  # Params:
 
-slack <- c(4000, 4000)
+  # pop: vector of populations
+  # utility: list of utility functions
+  # proposal.dist: a function that takes three parameters:
+  #   value, value conditioned on, and mode;
+  #   mode can take the value 'r' or 'd'. The former draws from the distribution while
+  #   the latter returns the probability of a value according to the proposal.
+  # move.cost: list of move cost functions b/w regions (assumes flow is uni-directional)
 
-capjobs <- 0.98 * capacity
+  # returns population counts for each region
 
-UE <- 1 - (capjobs / capacity)
-
-avgUE <- sum((capacity / totalPop) * UE)
-
-
-marginaljobs <- function(p,  capacity,  slack) {
-  return((capacity - p) / (capacity - slack))
-}
-
-intmarginaljobs <- function(p,  capacity,  slack) {
-  imj <- (2 * capacity * p - p^2) / (2 * capacity - 2 * slack)
-  return(imj)
-}
-
-numjobsraw <- function(p1, p2, capacity, slack) {
-  nj <- intmarginaljobs(p2, capacity, slack) -
-    intmarginaljobs(p1, capacity, slack)
-  return(nj)
-}
-
-numjobs <- function(p1, p2, capacity, slack) {
-  if (p1 < slack) {
-    if (p2 < slack) {
-      return((p2-p1))
-    } else if (p2 >= slack && p2 < capacity) {
-      nj <- (slack-p1) + numjobsraw(slack, p2, capacity, slack)
-      return(nj)
-    } else {
-      nj <- (slack-p1) + numjobsraw(slack, capacity, capacity, slack)
-      return(nj)
-    }
-
-  } else if (p1 >= slack && p1 < capacity) {
-    if (p2 < slack) {
-      nj <- (p2-slack) + numjobsraw(p1, slack, capacity, slack)
-      return(nj)
-    } else if (p2 >= slack && p2 < capacity) {
-      nj <- numjobsraw(p1, p2, capacity, slack)
-      return(nj)
-    } else {
-      nj <- numjobsraw(p1, capacity, capacity, slack)
-      return(nj)
-    }
-
-  } else {
-    if (p2 < slack) {
-      nj <- numjobsraw(capacity, slack, capacity, slack) + (p2 - slack)
-      return(nj)
-    } else if (p2 >= slack && p2 < capacity) {
-      nj <- numjobsraw(capacity, p2, capacity, slack)
-      return(nj)
-    } else {
-      return(0)
-    }
-  }
-}
-
-# what happens to the average unemployment rate if we move 500 people from one region to another?
-deltaPop <- c(-500, 500)
-deltaJobs <- c(numjobs(5000, 4500, 5000, 4000), numjobs(5000, 5500, 5000, 4000))
-
-population <- capacity + deltaPop
-jobs <- capjobs + deltaJobs
-UE <- 1-(jobs / population)
-avgUE <- sum((capacity / totalPop) * UE)
-
-avgUE
-
-# this shows what happens when people move from a productive configuration to a non-productive configuration. Overall unemployment increases. Given this new configuration,  500 people would be better off if they moved back to region A.
-
-# Now to try to apply MCMC to the pan model. First, just a repeat of how this would run (with larger populations).
-
-# First, how it runs towards equilibrium as a markov chain:
-
-u1 <- function(p) {
-  return(-p[1]+8000)
-}
-
-u2 <- function(p) {
-  return(-p[2]+14000)
-}
-
-c12 <- function(f) {
-  return(2 * f[1])
-}
-
-c21 <- function(f) {
-  return(2 * f[2])
-}
-
-p0 <- c(4000, 2000)
-
-flowcalc <- function(p) {
-  if (u2(p) > u1(p)) {
-    f1 <- (p[1] - p[2] + 6000) / 4
-    f <- c(f1, 0)
-  } else if (u1(p) > u2(p)) {
-    f2 <- (p[2] - p[1] - 6000) / 4
-    f <- c(0, f2)
-  } else {
-    f <- c(0, 0)
+  # pan-nagurney utility-cost model helper function
+  pn.prob <- function(src.utility, dest.utility, cost) {
+    (dest.utility - cost) / src.utility
   }
 
-  return(f)
-}
+  # randomly select source and destination regions
+  num.regions <- length(pop)
+  rand.ints <- sample(1:num.regions, 2, FALSE)
+  region.src <- rand.ints[1]
+  region.dest <- rand.ints[2]
 
-pdata <- data.frame(p1 = 4000,  p2 = 2000,  u1 = u1(p0),  u2 = u2(p0))
+  # create pop, utility, move cost lists for src and dest regions
+  p <- list(src=pop[region.src], dest=pop[region.dest])
+  u <- list(src=utility[[region.src]], dest=utility[[region.dest]])
+  m <- move.cost[[region.src]][[region.dest]]
 
+  # generate flow from proposal distribution
+  f <- proposal.dist(p$src, 0, 'r')
 
-for(i in 1:10) {
-  p <- c(tail(pdata$p1, 1), tail(pdata$p2, 1))
-  f <- flowcalc(p)
-  u <- c(u1(p), u2(p))
-  totu <- sum(p * u)
+  # calculate whether to accept this step using M-H
+  cost <- m(f)
+  prob0 <- pn.prob(u$src(p$src), u$dest(p$dest), 0)  # prob before move
+  prob1 <- pn.prob(u$src(p$src - f), u$dest(p$dest + f), cost)  # prob after
+  prop.num <- proposal.dist(p$src, p$src - f, 'd')
+  prop.denom <- proposal.dist(p$src - f, p$src, 'd')
 
-  pdata <- rbind(pdata,  c(p[1]-f[1]+f[2],  p[2]+f[1]-f[2],  u))
-}
-
-# Now lets try to run this as MCMC. for this large a population,  the
-
-p0 <- c(4000, 2000)
-pdata <- data.frame(p1 = 4000,  p2 = 2000,  u1 = u1(p0),  u2 = u2(p0))
-
-pdatastep <- function(pdata) {
-
-  location <- sample(c(1, 2), 1)
-
-  p <- c(tail(pdata$p1, 1), tail(pdata$p2, 1))
-
-  if (location == 1) {
-    if (p[1] >= 10 && p[2] <= 5990) {
-      if (u1(p) < u2(p) - 200) {
-        p[1] <- p[1]-10
-        p[2] <- p[2]+10
-        pdata <- rbind(pdata, c(p, u1(p), u2(p)))
-      } else {
-        probmove <- (u2(p) - 200) / u1(p)
-        if (runif (1) < probmove) {
-          p[1] <- p[1]-10
-          p[2] <- p[2]+10
-          pdata <- rbind(pdata, c(p, u1(p), u2(p)))
-        } else {
-          pdata <- rbind(pdata, c(p, u1(p), u2(p)))
-        }
-      }
-    } else {
-      pdata <- rbind(pdata, c(p, u1(p), u2(p)))
-    }
+  # a <- min(1, (prob1 * prop.num) / (prob0 * prop.denom))
+  a <- min(1, prob1 * (prop.num / prop.denom))
+  if ((a > 1 | runif(1) < a) & (p$src - f > 0)) {
+    pop[region.src] <- p$src - f
+    pop[region.dest] <- p$dest + f
   }
 
-  if (location == 2) {
-    if (p[2] >= 10 && p[1] <= 5990) {
-      if (u2(p) < u1(p) - 200) {
-        p[2] <- p[2]-10
-        p[1] <- p[1]+10
-        pdata <- rbind(pdata, c(p, u1(p), u2(p)))
-      } else {
-        probmove <- (u1(p) - 200) / u2(p)
-        if (runif (1) < probmove) {
-          p[2] <- p[2]-10
-          p[1] <- p[1]+10
-          pdata <- rbind(pdata, c(p, u1(p), u2(p)))
-        } else {
-          pdata <- rbind(pdata, c(p, u1(p), u2(p)))
-        }
-      }
-    } else {
-      pdata <- rbind(pdata, c(p, u1(p), u2(p)))
-    }
-  }
-
-  return(pdata)
-
+  return(pop)
 }
-
-iter <- 1:10000
-for(i in iter) {
-  pdata <- pdatastep(pdata)
-}
-
-tail(pdata)
-
-# do the population estimates converge?
-plot(iter, pdata$p1[-1], type = 'l')
-plot(iter, pdata$p2[-1], type = 'l')
